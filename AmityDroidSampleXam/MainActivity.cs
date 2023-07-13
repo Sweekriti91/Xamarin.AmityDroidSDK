23c2d6341493118f8a72a2ecc05bdbca18b3f11b
Sweekriti Satpathy <swsat@microsoft.com>
2023-07-12 17:58:28 -0700

    more APIs lighting up, fixing missing resources errors

--- AmityDroidSampleXam/MainActivity.cs
+++ AmityDroidSampleXam/MainActivity.cs
@@ -2,9 +2,8 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
-using Com.Amity.Socialcloud.Uikit;
 
-namespace AmityDroidXamSample
+namespace AmityDroidSampleXam
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
@@ -13,15 +12,6 @@ namespace AmityDroidXamSample
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-
-        //    var endpoint = ""“https://api.us.amity.co”";
-        //    //Authenticate Amity via ApiKey , 
-        //    AmityCoreClient.setup(
-        //    apiKey = "b0edec5932dff8304f658c4d060a178cd858deebb3673e29",
-        //    httpEndpoint = ""“https://api.us.amity.co”",  // optional param, defaulted as SG region 
-        //    socketEndpoint = “https://api.us.amity.co” // optional param, defaulted as SG region 
-        //)
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
--- AmityDroidSampleXam/Properties/AssemblyInfo.cs
+++ AmityDroidSampleXam/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AmityDroidXamSample")]
+[assembly: AssemblyTitle("AmityDroidSampleXam")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AmityDroidXamSample")]
+[assembly: AssemblyProduct("AmityDroidSampleXam")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
