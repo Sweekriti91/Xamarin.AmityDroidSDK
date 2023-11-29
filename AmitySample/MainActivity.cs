b1b954767aae52eb450840f17eaf9d4764bd97c5
Sweekriti Satpathy <swsat@microsoft.com>
2023-11-28 16:11:07 -0800

    checking

--- AmitySample/MainActivity.cs
+++ AmitySample/MainActivity.cs
@@ -1,4 +1,9 @@
-﻿namespace AmitySample;
+﻿
+//using Com.Amity.Socialcloud.Sdk.Api.Core;
+//using Com.Amity.Socialcloud.Sdk.Api.Core.Endpoint;
+
+namespace AmitySample;
+
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : Activity
@@ -6,6 +11,8 @@ public class MainActivity : Activity
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
+
+        //AmityCoreClient
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
