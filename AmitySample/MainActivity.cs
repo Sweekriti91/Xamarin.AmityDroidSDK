27510bb7863711ddb9237268d35045c7f87eea62
Sweekriti Satpathy <swsat@microsoft.com>
2023-12-11 10:37:51 -0800

    builds now no error

--- AmitySample/MainActivity.cs
+++ AmitySample/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Com.Amity.Socialcloud.Sdk.Api.Core;
 using Com.Amity.Socialcloud.Sdk.Api.Core.Encryption;
 using Com.Amity.Socialcloud.Sdk.Api.Core.Endpoint;
+using Com.Amity.Socialcloud.Sdk.Model.Core.Session;
 
 namespace AmitySample;
 
@@ -10,9 +11,21 @@ public class MainActivity : Activity
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-        _ = AmityCoreClient.Instance.Setup("", AmityEndpoint.US.Instance, AmityDBEncryption.NONE.Instance).Subscribe();
+        AmityCoreClient.Instance.Setup("APIKEY", AmityEndpoint.US.Instance, AmityDBEncryption.NONE.Instance).Subscribe();
+
+        System.Diagnostics.Debug.WriteLine("STATUS :: " + AmityCoreClient.Instance.CurrentSessionState.ToString());
+
+        //AmityCoreClient.Instance.Login("academy9989",new Session )
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
     }
+
+    public partial class MySessionHandler : Com.Amity.Socialcloud.Sdk.Model.Core.Session.
+    {
+
+    }
 }
+
+
+
